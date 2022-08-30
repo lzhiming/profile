eaf145e2289e9248a370a8b37eb49e0c2ea9e55f
lzhiming <l350709947@163.com>
2022-08-30 14:50:43 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+	<img alt="Vue logo" src="../assets/logo.png" />
+    <h1>{{ msg }}</h1>
+
+    <button @click="count++">count is: {{ count }}</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const props = withDefaults(defineProps<{
+    msg: string;
+}>(), {})
+
+const count = ref(0);
+
+</script>
+
+<style scoped>
+a {
+    color: #42b983;
+}
+
+label {
+    margin: 0 0.5em;
+    font-weight: bold;
+}
+
+code {
+    background-color: #eee;
+    padding: 2px 4px;
+    border-radius: 4px;
+    color: #304455;
+}
+</style>
---
+++ src/views/Example.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <el-button type="primary" round @click="changeLanguage">
+            {{ $t('buttons.changeLanguage') }}
+        </el-button>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<script setup lang="ts">
+import { useStore } from '@/store';
+import { i18n, setLanguage } from '@/i18n';
+
+function changeLanguage() {
+    setLanguage(i18n.global.locale === "zh-cn" ? 'en' : 'zh-cn')
+}
+
+const st = useStore();
+
+</script>
