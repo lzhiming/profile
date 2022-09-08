de42d1b3b9e0491ddde8d8fc40e65a5174b35d4e
lzhiming <l350709947@163.com>
2022-09-08 16:49:09 +0800

    Create header.vue

---
+++ src/components/header.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="header">
+		<div class="title">
+            LZMING
+        </div>
+        <a href="https://github.com/lzhiming" target="_blank" rel="noopener noreferrer">            
+            <img class="is-icon" src="../assets/fluidicon.png" alt="">
+        </a>
+	</div>
+</template>
+
+<style scoped lang="scss">
+    .header{
+        display: flex;
+        height: 50px;
+        padding: 7px;
+        justify-content: space-between;
+        align-items: center;
+        box-shadow: 0px 3px 5px -2px rgb(5 0 10 / 40%);
+        .title{
+            font-size: 2rem;
+            font-weight: 500;
+        }
+    }
+    .is-icon{
+        width: 36px; height: 36px;
+    }
+</style>
