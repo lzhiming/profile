ca3d0cb484d91a0ad07b9cca561b1a6d7d92294c
lzhiming <l350709947@163.com>
2022-09-08 16:49:20 +0800

    create index

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,17 +1,38 @@
 <template>
-	<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<Header />
+	<div class="pro-grid" v-show="isLoading">
+		<div v-for="pro in prolist" :key="pro" class="loading-card card-skeleton"></div>
+	</div>
+	<div class="pro-grid" v-show="!isLoading">
+
+		<Project v-for="pro in prolist" :key="pro" title="1" msg="Hello Vue 3 + TypeScript + Vite" />
+	</div>
+	
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+@import './css/index.scss';
 #admin { width: 100%; min-height: 100vh; height: 100vh; }
 .top { height: 20px; }
 
 .main { height: calc(100% - 40px); }
 .main .nav { width: 200px; }
 .main .tabs { width: calc(100% - 200px); }
+
 </style>
 
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
+import Project from '@/components/project.vue'
+import Header from '@/components/header.vue'
+import { onMounted, ref } from 'vue';
 
+const prolist = ['1', '2', '3', '4','5', '6', '7', '8','9', '10', '11', '12']
+
+let isLoading = ref(true)
+
+onMounted(() => {
+	setTimeout(() => {
+		isLoading.value = false
+	}, 2000)
+})
 </script>
